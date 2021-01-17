45b5c79aa4ab626502c980710a09994db2b106db
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-01-17 17:23:05 -0500

    added category

--- models/Score.js
+++ models/Score.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const ScoreSchema = mongoose.Schema({
   nickname: { type: String, required: true },
-  trivia_duration: { type: String, required: true },
+  category: { type: String, required: true },
   correct_answers: { type: String, required: true },
+  trivia_duration: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Score", ScoreSchema);
+module.exports = mongoose.model("Score", ScoreSchema);t
